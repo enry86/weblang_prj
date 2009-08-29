83fd388d912dff708ac22b71364f6c89a34f845a
Enrico Sartori <sartori.enrico@gmail.com>
2009-08-29 17:59:21 +0200

    person ready, group almost

--- src/weblanguages/project/client/gui/CgPanel.java
+++ src/weblanguages/project/client/gui/CgPanel.java
@@ -1,6 +1,9 @@
 package weblanguages.project.client.gui;
 
 import it.cg.wl.sartoriWLC.Group;
+import it.cg.wl.sartoriWLC.GroupLabel;
+import it.cp.wl.sartoriWLC.PersonLabel;
+
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -12,7 +15,11 @@ import weblanguages.project.client.Connector;
 public class CgPanel extends JPanel{
 	int num = 3;
 	JTextField[] params;
+	JTextField[] upd_p;
+	JTextField[] l_params;
+	JTextField[] l_upd_p;
 	JButton ins, del, upd, src;
+	JButton bins, bdel, bupd, bsrc;
 	JLabel status;
 	Connector c;
 	CgListener cpl;
@@ -24,11 +31,16 @@ public class CgPanel extends JPanel{
 		cpl = new CgListener(this);
 		setLayout(new BorderLayout());
 		params = new JTextField[num];
+		upd_p = new JTextField[num];
+		l_params = new JTextField[2];
+		l_upd_p = new JTextField[2];
 		tb = new JTextArea();
 		JScrollPane sp = new JScrollPane(tb);
 		add(sp, BorderLayout.CENTER);
 		JPanel top = set_top_panel();
 		add(top, BorderLayout.NORTH);
+		JPanel bot = set_bot_panel();
+		add(bot, BorderLayout.SOUTH);
 		setVisible(true);
 	}
 	
@@ -57,11 +69,51 @@ public class CgPanel extends JPanel{
 		return res;
 	}
 	
-	public Group get_group(){
+	private JPanel set_bot_panel(){
+		JPanel res = new JPanel();
+		JPanel but = new JPanel();
+		JPanel fie = new JPanel();
+		JPanel ufie =new JPanel();
+		bins = new JButton("Insert");
+		bdel = new JButton("Delete");
+		bsrc = new JButton("Search");
+		bupd = new JButton("Update");
+		fie.add(new JLabel("Select: "));
+		ufie.add(new JLabel("Update with: "));
+		for (int i = 0; i < 2; i++){
+			l_params[i] = new JTextField(10);
+			l_upd_p[i] = new JTextField(10);
+			fie.add(l_params[i]);
+			ufie.add(l_upd_p[i]);
+		}
+		but.add(bupd);
+		but.add(bins);
+		but.add(bdel);
+		but.add(bsrc);
+		but.add(status);
+		bupd.addMouseListener(cpl);
+		bins.addMouseListener(cpl);
+		bdel.addMouseListener(cpl);
+		bsrc.addMouseListener(cpl);		
+		res.setLayout(new BorderLayout());
+		res.add(fie, BorderLayout.NORTH);
+		res.add(ufie, BorderLayout.CENTER);
+		res.add(but, BorderLayout.SOUTH);
+		return res;
+	}
+	
+	public Group get_group(JTextField[] params){
 		Group g = new Group();
 		if (params[0].getText().compareTo("") != 0) g.setId_group(Integer.parseInt(params[0].getText()));
 		if (params[1].getText().compareTo("") != 0) g.setGroup_name(params[1].getText());
 		if (params[2].getText().compareTo("") != 0) g.setGroup_uri(params[2].getText());
+		return g;
+	}
+	
+	public GroupLabel get_group_l(JTextField[] params){
+		GroupLabel g = new GroupLabel();
+		if (params[0].getText().compareTo("") != 0) g.setId_group(Integer.parseInt(params[0].getText()));
+		if (params[1].getText().compareTo("") != 0) g.setId_label(Integer.parseInt(params[1].getText()));
 		return g;
 	}
 	
@@ -71,6 +123,16 @@ public class CgPanel extends JPanel{
 			tmp += g[i].getId_group() + ",\t";
 			tmp += g[i].getGroup_name() + ",\t";
 			tmp += g[i].getGroup_uri() + ",\t";
+			tmp += "\n";
+		}
+		tb.setText(tmp);
+	}
+	
+	public void show_values_l(GroupLabel[] g){
+		String tmp = "";
+		for (int i = 0; i < g.length; i++){
+			tmp += g[i].getId_group() + ",\t";
+			tmp += g[i].getId_label() + ",\t";
 			tmp += "\n";
 		}
 		tb.setText(tmp);
@@ -86,7 +148,7 @@ public class CgPanel extends JPanel{
 				boolean res;
 				try {
 					status.setText("Loading...");
-					res = c.go.createGroup(p.get_group());
+					res = c.go.createGroup(p.get_group(p.params));
 					if (res) status.setText("OK");
 					else status.setText("ERR");
 				} catch (RemoteException e1) {
@@ -98,7 +160,7 @@ public class CgPanel extends JPanel{
 				try {
 					status.setText("Loading...");
 					status.updateUI();
-					res = c.go.readGroup(p.get_group());
+					res = c.go.readGroup(p.get_group(p.params));
 					status.setText("OK");
 					p.show_values(res);
 				} catch (RemoteException e1) {
@@ -110,9 +172,65 @@ public class CgPanel extends JPanel{
 				try {
 					status.setText("Loading...");
 					status.updateUI();
-					res = c.go.deleteGroup(p.get_group());
+					res = c.go.deleteGroup(p.get_group(p.params));
 					if (res) status.setText("OK");
 					else status.setText("ERR");
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else if (e.getSource() == p.upd){
+				boolean res;
+				try {
+					status.setText("Loading...");
+					status.updateUI();
+					res = c.go.updateGroup(p.get_group(p.params), p.get_group(p.upd_p));
+					if (res) status.setText("OK");
+					else status.setText("ERR");
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else if (e.getSource() == p.bins){
+				boolean res;
+				try {
+					status.setText("Loading...");
+					res = c.go.createGroupLabel(p.get_group_l(p.l_params));
+					if (res) status.setText("OK");
+					else status.setText("ERR");
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else if (e.getSource() == p.bdel){
+				boolean res;
+				try {
+					status.setText("Loading...");
+					res = c.go.deleteGroupLabel(p.get_group_l(p.l_params));
+					if (res) status.setText("OK");
+					else status.setText("ERR");
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else if (e.getSource() == p.bupd){
+				boolean res;
+				try {
+					status.setText("Loading...");
+					res = c.go.updateGroupLabel(p.get_group_l(p.l_params), p.get_group_l(p.l_upd_p));
+					if (res) status.setText("OK");
+					else status.setText("ERR");
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else if (e.getSource() == p.bsrc){
+				GroupLabel[] res;
+				try {
+					status.setText("Loading...");
+					res = c.go.readGroupLabel(p.get_group_l(p.l_params));
+					status.setText("OK");
+					p.show_values_l(res);
 				} catch (RemoteException e1) {
 					e1.printStackTrace();
 				}
--- src/weblanguages/project/client/gui/CpPanel.java
+++ src/weblanguages/project/client/gui/CpPanel.java
@@ -1,6 +1,7 @@
 package weblanguages.project.client.gui;
 
 import it.cp.wl.sartoriWLC.Person;
+import it.cp.wl.sartoriWLC.PersonLabel;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
@@ -9,13 +10,18 @@ import java.awt.event.MouseAdapter;
 import java.rmi.RemoteException;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 
 import weblanguages.project.client.Connector;
 
 public class CpPanel extends JPanel{
 	int num = 7;
 	JTextField[] params;
+	JTextField[] upd_p;
+	JTextField[] l_params;
+	JTextField[] l_upd_p;
 	JButton ins, del, upd, src;
+	JButton bins, bdel, bupd, bsrc;
 	JLabel status;
 	Connector c;
 	CpListener cpl;
@@ -27,11 +33,16 @@ public class CpPanel extends JPanel{
 		cpl = new CpListener(this);
 		setLayout(new BorderLayout());
 		params = new JTextField[num];
+		upd_p = new JTextField[num];
+		l_params = new JTextField[2];
+		l_upd_p = new JTextField[2];
 		tb = new JTextArea();
 		JScrollPane sp = new JScrollPane(tb);
 		add(sp, BorderLayout.CENTER);
 		JPanel top = set_top_panel();
 		add(top, BorderLayout.NORTH);
+		JPanel bot = set_bot_panel();
+		add(bot, BorderLayout.SOUTH);
 		setVisible(true);
 	}
 	
@@ -40,27 +51,77 @@ public class CpPanel extends JPanel{
 		JPanel res = new JPanel();
 		JPanel but = new JPanel();
 		JPanel fie = new JPanel();
+		JPanel ufie = new JPanel();
+		JLabel slab = new JLabel("Select: ");
+		JLabel ulab = new JLabel("Update with: ");
+		Border bor = BorderFactory.createTitledBorder("Person");
+		upd = new JButton("Update");
 		ins = new JButton("Insert");
 		del = new JButton("Delete");
 		src = new JButton("Search");
+		fie.add(slab);
+		ufie.add(ulab);
 		for (int i = 0; i < num; i++){
-			params[i] = new JTextField(10);
+			params[i] = new JTextField(6);
+			upd_p[i] = new JTextField(6);
+			ufie.add(upd_p[i]);
 			fie.add(params[i]);
 		}
+		but.add(upd);
 		but.add(ins);
 		but.add(del);
 		but.add(src);
 		but.add(status);
+		upd.addMouseListener(cpl);
 		ins.addMouseListener(cpl);
 		del.addMouseListener(cpl);
 		src.addMouseListener(cpl);		
 		res.setLayout(new BorderLayout());
 		res.add(fie, BorderLayout.NORTH);
+		res.add(ufie, BorderLayout.CENTER);
 		res.add(but, BorderLayout.SOUTH);
+		res.setBorder(bor);
 		return res;
 	}
 	
-	public Person get_person(){
+	private JPanel set_bot_panel(){
+		JPanel res = new JPanel();
+		JPanel but = new JPanel();
+		JPanel fie = new JPanel();
+		JPanel ufie = new JPanel();
+		JLabel slab = new JLabel("Select: ");
+		JLabel ulab = new JLabel("Update with: ");
+		Border bor = BorderFactory.createTitledBorder("Person Label");
+		bupd = new JButton("Update");
+		bins = new JButton("Insert");
+		bdel = new JButton("Delete");
+		bsrc = new JButton("Search");
+		fie.add(slab);
+		ufie.add(ulab);
+		for (int i = 0; i < 2; i++){
+			l_params[i] = new JTextField(6);
+			l_upd_p[i] = new JTextField(6);
+			ufie.add(l_upd_p[i]);
+			fie.add(l_params[i]);
+		}
+		but.add(bupd);
+		but.add(bins);
+		but.add(bdel);
+		but.add(bsrc);
+		but.add(status);
+		bupd.addMouseListener(cpl);
+		bins.addMouseListener(cpl);
+		bdel.addMouseListener(cpl);
+		bsrc.addMouseListener(cpl);		
+		res.setLayout(new BorderLayout());
+		res.add(fie, BorderLayout.NORTH);
+		res.add(ufie, BorderLayout.CENTER);
+		res.add(but, BorderLayout.SOUTH);
+		res.setBorder(bor);
+		return res;
+	}
+	
+	public Person get_person(JTextField[] params){
 		Person p = new Person();
 		if (params[0].getText().compareTo("") != 0) p.setId_person(Integer.parseInt(params[0].getText()));
 		if (params[1].getText().compareTo("") != 0) p.setFirst_name(params[1].getText());
@@ -72,16 +133,34 @@ public class CpPanel extends JPanel{
 		return p;
 	}
 	
+	
+	public PersonLabel get_person_l(JTextField[] params){
+		PersonLabel p = new PersonLabel();
+		if (params[0].getText().compareTo("") != 0) p.setId_user(Integer.parseInt(params[0].getText()));
+		if (params[1].getText().compareTo("") != 0) p.setId_label(Integer.parseInt(params[1].getText()));
+		return p;
+	}
+	
 	public void show_values(Person[] p){
 		String tmp = "";
 		for (int i = 0; i < p.length; i++){
-			tmp += p[i].getId_person() + ", ";
-			tmp += p[i].getFirst_name() + ", ";
-			tmp += p[i].getLast_name() + ", ";
-			tmp += p[i].getCitizenship() + ", ";
-			tmp += p[i].getTitle() + ", ";
-			tmp += p[i].getEmail() + ", ";
-			tmp += p[i].getPerson_uri() + ", ";
+			tmp += p[i].getId_person() + ",\t";
+			tmp += p[i].getFirst_name() + ",\t";
+			tmp += p[i].getLast_name() + ",\t";
+			tmp += p[i].getCitizenship() + ",\t";
+			tmp += p[i].getTitle() + ",\t";
+			tmp += p[i].getEmail() + ",\t";
+			tmp += p[i].getPerson_uri() + ",\t";
+			tmp += "\n";
+		}
+		tb.setText(tmp);
+	}
+	
+	public void show_values_l(PersonLabel[] p){
+		String tmp = "";
+		for (int i = 0; i < p.length; i++){
+			tmp += p[i].getId_user() + ",\t";
+			tmp += p[i].getId_label() + ",\t";
 			tmp += "\n";
 		}
 		tb.setText(tmp);
@@ -97,20 +176,86 @@ public class CpPanel extends JPanel{
 				boolean res;
 				try {
 					status.setText("Loading...");
-					res = c.po.createPerson(p.get_person());
+					res = c.po.createPerson(p.get_person(params));
 					if (res) status.setText("OK");
 					else status.setText("ERR");
 				} catch (RemoteException e1) {
 					e1.printStackTrace();
 				}
 			}
-			if (e.getSource() == p.src){
+			else if (e.getSource() == p.del){
+				boolean res;
+				try {
+					status.setText("Loading...");
+					res = c.po.deletePerson(p.get_person(params));
+					if (res) status.setText("OK");
+					else status.setText("ERR");
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else if (e.getSource() == p.upd){
+				boolean res;
+				try {
+					status.setText("Loading...");
+					res = c.po.updatePerson(p.get_person(p.params), p.get_person(p.upd_p));
+					if (res) status.setText("OK");
+					else status.setText("ERR");
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else if (e.getSource() == p.src){
 				Person[] res;
 				try {
 					status.setText("Loading...");
-					res = c.po.readPerson(p.get_person());
+					res = c.po.readPerson(p.get_person(params));
 					status.setText("OK");
 					p.show_values(res);
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else if (e.getSource() == p.bins){
+				boolean res;
+				try {
+					status.setText("Loading...");
+					res = c.po.createPersonLabel(p.get_person_l(p.l_params));
+					if (res) status.setText("OK");
+					else status.setText("ERR");
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else if (e.getSource() == p.bdel){
+				boolean res;
+				try {
+					status.setText("Loading...");
+					res = c.po.deletePersonLabel(p.get_person_l(p.l_params));
+					if (res) status.setText("OK");
+					else status.setText("ERR");
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else if (e.getSource() == p.bupd){
+				boolean res;
+				try {
+					status.setText("Loading...");
+					res = c.po.updatePersonLabel(p.get_person_l(p.l_params), p.get_person_l(p.l_upd_p));
+					if (res) status.setText("OK");
+					else status.setText("ERR");
+				} catch (RemoteException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else if (e.getSource() == p.bsrc){
+				PersonLabel[] res;
+				try {
+					status.setText("Loading...");
+					res = c.po.readPersonLabel(p.get_person_l(p.l_params));
+					status.setText("OK");
+					p.show_values_l(res);
 				} catch (RemoteException e1) {
 					e1.printStackTrace();
 				}
